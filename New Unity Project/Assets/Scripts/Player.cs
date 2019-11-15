500fcddb1898365ff0c35c9aa00ae33463c74862
ninjerie <38001901+ninjerie@users.noreply.github.com>
2019-11-15 17:13:30 +0000

    Added basic movement
    Can move along x & z axis. Now need to implement mouse

---
+++ New Unity Project/Assets/Scripts/Player.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    public float speed = 5;
+    private Rigidbody rb;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void FixedUpdate()
+    {
+        //gets input along x-axis
+        float x = Input.GetAxis("Horizontal");
+        //gets input along y-axis
+        float y = Input.GetAxis("Vertical");
+
+        //sets velocity * speed
+        rb.velocity = new Vector3(x, 0, y) * speed;
+        
+    }
+}
