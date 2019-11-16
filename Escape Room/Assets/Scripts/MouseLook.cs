48fb30c5a168f7bc80520810300f4699ad27f08f
ninjerie <38001901+ninjerie@users.noreply.github.com>
2019-11-16 23:40:18 +0000

    Character looking and movement complete
    Camera working how I want it to including restraints on head movements.

---
+++ Escape Room/Assets/Scripts/MouseLook.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MouseLook : MonoBehaviour
+{
+    public float minY = -30f;
+    public float maxY = 30f;
+
+    private Vector2 mouseDirection;
+    private Transform body;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        Cursor.lockState = CursorLockMode.Locked;
+        Cursor.visible = true;
+
+        body = this.transform.parent.transform;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {        
+        Vector2 mouseChange = new Vector2(Input.GetAxisRaw("Mouse X"), Input.GetAxisRaw("Mouse Y"));
+        mouseDirection += mouseChange;
+        mouseDirection.y = Mathf.Clamp(mouseDirection.y, minY, maxY);
+
+        this.transform.localRotation = Quaternion.AngleAxis(-mouseDirection.y, Vector3.right);
+        body.localRotation = Quaternion.AngleAxis(mouseDirection.x, Vector3.up);
+        
+    }
+}
---
+++ Escape Room/Assets/Scripts/Player.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    public float speed = 5;
+    private Rigidbody rb;
+    private Vector3 moveDirection;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //gets input along x-axis
+        float x = Input.GetAxis("Horizontal");
+        x *= Time.deltaTime;
+        //gets input along y-axis
+        float y = Input.GetAxis("Vertical");
+        y *= Time.deltaTime;
+
+        moveDirection = (x * transform.right + y * transform.forward).normalized;
+    }
+
+    private void FixedUpdate()
+    {
+        Vector3 correctYVelocity = new Vector3(0, rb.velocity.y, 0);
+        rb.velocity = moveDirection * speed * Time.deltaTime;
+        // move direction keeps setting y velocity to 0
+        // to get the player to fall correctly we need to add it back
+        rb.velocity += correctYVelocity;
+    }
+}
