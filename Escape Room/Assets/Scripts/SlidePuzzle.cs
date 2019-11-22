cc5be0f1f74c77fb7c11cb323a5a232c26b7814a
ninjerie <38001901+ninjerie@users.noreply.github.com>
2019-11-22 17:12:12 +0000

    Adjusting slide puzzle
    Created a temp build to send to people to check slide puzzle is completable.

--- Escape Room/Assets/Scripts/SlidePuzzle.cs
+++ Escape Room/Assets/Scripts/SlidePuzzle.cs
@@ -4,9 +4,19 @@ using UnityEngine;
 
 public class SlidePuzzle : MonoBehaviour
 {
-    public Transform slot;
-    private float xtemp;
-    private float ytemp;
+    public Transform tile1, tile2, tile3, tile4, tile5, tile6, tile7, tile8, tile9;
+    public GameObject slot;
+
+    private Vector3 position1 = new Vector3(1, 9, 0);
+    private Vector3 position2 = new Vector3(2, 9, 0);
+    private Vector3 position3 = new Vector3(3, 9, 0);
+    private Vector3 position4 = new Vector3(1, 8, 0);
+    private Vector3 position5 = new Vector3(2, 8, 0);
+    private Vector3 position6 = new Vector3(3, 8, 0);
+    private Vector3 position7 = new Vector3(1, 7, 0);
+    private Vector3 position8 = new Vector3(2, 7, 0);
+    private Vector3 position9 = new Vector3(3, 7, 0);
+
 
     // Start is called before the first frame update
     void Start()
@@ -20,16 +30,12 @@ public class SlidePuzzle : MonoBehaviour
         
     }
 
-    private void OnMouseUp()
+    bool CheckTile()
     {
-        if (Vector3.Distance(this.transform.position, slot.position) == 1)
+        if (tile1.position == position1)
         {
-            xtemp = this.transform.position.x;
-            ytemp = this.transform.position.y;
 
-            // 6 should be replaced with whatever the z value of the group holding the tiles is
-            this.transform.position = new Vector3(slot.position.x, slot.position.y, 6f);
-            slot.position = new Vector3(xtemp, ytemp, 6f);
         }
+        return true;
     }
 }
---
+++ Escape Room/Assets/Scripts/TileMove.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TileMove : MonoBehaviour
+{
+    public Transform slot;
+    private float xtemp;
+    private float ytemp;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnMouseUp()
+    {
+        if (Vector3.Distance(this.transform.position, slot.position) == 1)
+        {
+            xtemp = this.transform.position.x;
+            ytemp = this.transform.position.y;
+
+            // 6 should be replaced with whatever the z value of the group holding the tiles is
+            this.transform.position = new Vector3(slot.position.x, slot.position.y, 6f);
+            slot.position = new Vector3(xtemp, ytemp, 6f);
+        }
+    }
+}
